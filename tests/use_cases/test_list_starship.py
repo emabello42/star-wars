b2df46c5ba2ea03844f32f05e370d99db41d455c
Emmanuel Bello <emabello42@gmail.com>
2020-11-27 23:20:24 +0100

    test: add test for ListStartship use case

---
+++ tests/use_cases/test_list_starship.py
@@ -0,0 +1,25 @@
+import pytest
+from starwars.domain.starship import Starship
+from starwars.use_cases.list_starship import ListStarshipUseCase
+from starwars.request_objects import StarshipRequestObject
+import uuid
+from unittest import mock
+
+
+@pytest.fixture
+def domain_starships():
+    starship1 = Starship(code=uuid.uuid4(), name="X-wing", hyperdrive_rating=1.0)
+    starship2 = Starship(code=uuid.uuid4(), name="Naboo Royal Starship", hyperdrive_rating=1.8)
+    starship3 = Starship(code=uuid.uuid4(), name="B-wing", hyperdrive_rating=2.0)
+    return [starship1, starship2, starship3]
+
+
+def test_list_starship_orderby_hyperdrive(domain_starships):
+    repo = mock.Mock()
+    repo.list_starships.return_value = domain_starships
+    starship_list_use_case = ListStarshipUseCase(repo)
+    request = StarshipRequestObject()
+    response = starship_list_use_case.execute(request)
+    assert bool(response) is True
+    repo.list_starships.assert_called()
+    assert response.value == domain_starships
