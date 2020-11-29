daff0a7aacab775a848210878ab29a9d95c8b6fb
Emmanuel Bello <emabello42@gmail.com>
2020-11-30 00:33:03 +0100

    chore: prepare for production env

--- starwars/app.py
+++ starwars/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import logging
 from starwars.rest import starship
-from starwars.flask_settings import DevConfig
+from starwars.flask_settings import ProdConfig
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='(%(threadName)-9s) %(message)s', )
 
-def create_app(config_object=DevConfig):
+
+def create_app(config_object=ProdConfig):
     app = Flask(__name__)
     app.config.from_object(config_object)
     app.register_blueprint(starship.blueprint)
